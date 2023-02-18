c57728aa9c04b83d4f0ac67b612ef857d551b5fd
Atsushi <atu729@icloud.com>
2023-02-18 23:57:06 +0900

    add validation function

--- src/routes/api.php
+++ src/routes/api.php
@@ -15,5 +15,5 @@ Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register
 //Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
-    Route::resource('/tasks', \App\Http\Controllers\tasksController::class);
+    Route::resource('/tasks', \App\Http\Controllers\TasksController::class);
 });
